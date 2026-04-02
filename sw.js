49b8fd59cde7484cf8c166948de0218d9ba883a6
noashamz <anwar@risda.gov.my>
2026-04-03 02:25:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,7 @@
-const cacheName = 'inventorisda-v4'; // Tukar nama cache untuk paksa update
-const staticAssets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo-risda.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(staticAssets);
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request);
+        })
+    );
 });
